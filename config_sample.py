a4a6f4cd29354e229ff3e6678ee3ea99f54121f5
Tom Barron <tom.barron@comcast.net>
2015-05-31 16:08:27 -0400

    Remove config.py from git, replace with config_sample.py

--- config_sample.py
+++ config_sample.py
@@ -2,8 +2,8 @@ import os
 dn = os.path.abspath(os.path.dirname(__file__))
 
 WTF_CRSF_ENABLED = True
-SECRET_KEY = 'frabjous-day'
+SECRET_KEY = 'dont-use-this-string'
 DATABASE_URI = 'sqlite:///flask-openid.db'
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(dn, 'fns.db')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(dn, 'flask-openid.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(dn, 'db_repository')
