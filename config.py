b82fbada5a6b072bba9763851275aa0e8196e3f0
Tom Barron <tom.barron@comcast.net>
2015-05-31 13:13:12 -0400

    Working on database management

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
+import os
+dn = os.path.abspath(os.path.dirname(__file__))
+
 WTF_CRSF_ENABLED = True
 SECRET_KEY = 'frabjous-day'
 DATABASE_URI = 'sqlite:///flask-openid.db'
 DEBUG = True
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(dn, 'fns.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(dn, 'db_repository')
