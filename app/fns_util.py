3a22b18bfb31ada9a02ce7eb0384ec0cedb32767
Tom Barron <tom.barron@comcast.net>
2015-05-31 13:23:46 -0400

    Factor out common utilities to fns_util.py

---
+++ app/fns_util.py
@@ -0,0 +1,14 @@
+def bm_test_list():
+    bml = [
+        {
+        'bm_name': 'Google',
+        'bm_url': 'http://www.google.com',
+        'bm_comment': 'my default search engine'
+        },
+        {
+        'bm_name': 'Workflowy',
+        'bm_url': 'http://www.workflowy.com',
+        'bm_comment': 'my organizer'
+        },
+        ]
+    return bml
