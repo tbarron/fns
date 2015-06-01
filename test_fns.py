5f721f16d3de9a06cad013adaff5307e5154b1dd
Tom Barron <tom.barron@comcast.net>
2015-06-01 06:38:28 -0400

    Adding unit tests

--- test_fns.py
+++ test_fns.py
@@ -1,7 +1,34 @@
 import sys
 import pprint
 import flask
-import app
+from app import app, init_db
+import os
+import pdb
+import tempfile
+
+class TestFNS:
+    def setup_method(self, foo):
+        self.db_fd, app.config['DATABASE'] = tempfile.mkstemp()
+        self.app = app.test_client()
+        init_db()
+
+    def teardown_method(self, foo):
+        os.close(self.db_fd)
+        os.unlink(app.config['DATABASE'])
+
+    def test_empty_db_root(self):
+        rv = self.app.get('/')
+        redirect_msg = "You should be redirected automatically to target URL:"
+        assert redirect_msg in rv.data
+        assert '<a href="/login">' in rv.data
+
+    def test_empty_db_login(self):
+        rv = self.app.get('/login')
+        expected = ['href="static/fns.css"',
+                    'name="login" class="fcenter"']
+        for exp in expected:
+            assert exp in rv.data
+
 
 def test_sys_path():
     """
