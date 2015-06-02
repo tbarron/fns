bcf7476b6ddc2a517b89dd9760ee7d2b5e079485
Tom Barron <tom.barron@comcast.net>
2015-06-02 07:49:15 -0400

    Collect helper methods for testing at bottom of test class

--- test_fns.py
+++ test_fns.py
@@ -7,14 +7,6 @@ import pdb
 import tempfile
 
 class TestFNS:
-    def setup_method(self, foo):
-        self.db_fd, app.config['DATABASE'] = tempfile.mkstemp()
-        self.app = app.test_client()
-        init_db()
-
-    def teardown_method(self, foo):
-        os.close(self.db_fd)
-        os.unlink(app.config['DATABASE'])
 
     def test_empty_db_root(self):
         rv = self.app.get('/')
@@ -28,6 +20,19 @@ class TestFNS:
                     'name="login" class="fcenter"']
         for exp in expected:
             assert exp in rv.data
+
+    # -------------------------------------------------------------------------
+    # Helper methods
+    # -------------------------------------------------------------------------
+    def setup_method(self, foo):
+        self.db_fd, app.config['DATABASE'] = tempfile.mkstemp()
+        self.app = app.test_client()
+        init_db()
+
+    # -------------------------------------------------------------------------
+    def teardown_method(self, foo):
+        os.close(self.db_fd)
+        os.unlink(app.config['DATABASE'])
 
 
 def test_sys_path():
