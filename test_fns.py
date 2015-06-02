9b887bb0f95bde83cd4c71932348d8bf4e81c494
Tom Barron <tom.barron@comcast.net>
2015-06-02 07:51:30 -0400

    Add tests for logged out urls
     - Add generic logged_out_url method as helper
     - Add test method for each url calling logged_out_url helper

--- test_fns.py
+++ test_fns.py
@@ -21,8 +21,32 @@ class TestFNS:
         for exp in expected:
             assert exp in rv.data
 
+    def test_logged_out_profile(self):
+        """
+        Hitting /profile when logged out should redirect to /login
+        """
+        self.logged_out_url('/profile')
+
+    def test_logged_out_bookmark(self):
+        """
+        Hitting /bookmark when logged out should redirect to /login
+        """
+        self.logged_out_url('/bookmark')
+
+    def test_logged_out_index(self):
+        """
+        Hitting /index when logged out should redirect to /login
+        """
+        self.logged_out_url('/index')
+
     # -------------------------------------------------------------------------
     # Helper methods
+    # -------------------------------------------------------------------------
+    def logged_out_url(self, url):
+        rv = self.app.get(url)
+        assert self.redirect_msg in rv.data
+        assert '<a href="/login">' in rv.data
+
     # -------------------------------------------------------------------------
     def setup_method(self, foo):
         self.db_fd, app.config['DATABASE'] = tempfile.mkstemp()
